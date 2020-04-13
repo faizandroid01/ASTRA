45fb69992def7061e47de4422f8131494e70ff34
faiz-pitavya <faizrocks9211@gmail.com>
2020-04-13 19:31:51 +0530

    Scanner 1 from zing

--- SCANNER/app/src/main/java/com/faiz/scanner/MainActivity.java
+++ SCANNER/app/src/main/java/com/faiz/scanner/MainActivity.java
@@ -1,19 +1,36 @@
 package com.faiz.scanner;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.faiz.astra.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.faiz.astra.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText qrText;
+    private Button scanQr, generateQr;
+    private ImageView qrCodeImage;
+    private TextView scannedText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +39,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        scanQr = findViewById(R.id.scan);
+        generateQr = findViewById(R.id.generateQrCode);
+        qrText = findViewById(R.id.textForQr);
+        qrCodeImage = findViewById(R.id.qrCode);
+        scannedText = findViewById(R.id.viewScannedText);
+
     }
 
     @Override
@@ -53,4 +68,51 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void generateQrCode(View view) {
+        if (!TextUtils.isEmpty(qrText.getText())) {
+
+            MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+            try {
+                BitMatrix bitMatrix = multiFormatWriter.encode(qrText.getText().toString(), BarcodeFormat.QR_CODE, 500, 500);
+                BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                qrCodeImage.setImageBitmap(bitmap);
+            } catch (WriterException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
+    public void scanQrCode(View view) {
+
+        IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        intentIntegrator.setCameraId(0);
+        intentIntegrator.setOrientationLocked(false);
+        intentIntegrator.setPrompt("Scanning");
+        intentIntegrator.setBeepEnabled(true);
+        intentIntegrator.setBarcodeImageEnabled(true);
+        intentIntegrator.initiateScan();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if (result != null) {
+
+            if (result.getContents() != null) {
+                scannedText.setText(result.getContents());
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+    }
 }
+
